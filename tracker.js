ff6e8d1728fd01e683d80921b2c981f2b7fcfbc6
Ben Lopata <benlopata@gmail.com>
2019-07-15 12:18:25 -0500

    Added SRP torrent-parser.js and util.js, build-out of functions in tracker.js.

--- tracker.js
+++ tracker.js
@@ -45,8 +45,65 @@ function buildConnReq() {
   return buf;
 }
 
-function parseConnResp(res) {}
+function parseConnResp(res) {
+  return {
+    action: res.readUInt32BE(0),
+    transactionId: res.readUInt32BE(4),
+    connectionId: res.slice(8)
+  };
+}
 
-function buildAnnounceReq(connId) {}
+function buildAnnounceReq(connId, torrent, port = 6881) {
+  const buf = Buffer.allocUnsafe(98);
 
-function parseAnnounceResp(res) {}
+  connId.copy(buf, 0);
+
+  buf.writeUInt32BE(1, 8);
+
+  crypto.randomBytes(4).copy(buf, 12);
+
+  torrentParser.infoHash(torrent).copy(buf, 16);
+
+  util.genId().copy(buf, 36);
+
+  Buffer.alloc(8).copy(buf, 56);
+
+  torrentParser.size(torrent).copy(buf, 64);
+
+  Buffer.alloc(8).copy(buf, 72);
+
+  buf.writeUInt32BE(0, 80);
+
+  buf.writeUInt32BE(0, 80);
+
+  crypto.randomBytes(4).copy(buf, 88);
+
+  buf.writeInt32BE(-1, 92);
+
+  buf.writeUInt16BE(port, 96);
+
+  return buf;
+}
+
+function parseAnnounceResp(res) {
+  function group(iterable, groupSize) {
+    let groups = [];
+    for (let i = 0; i < iterable.length; i += groupSize) {
+      groups.push(iterable.slice(i, i + groupSize));
+    }
+    return groups;
+  }
+
+  return {
+    action: res.readUInt32BE(0),
+    transactionId: res.readUInt32BE(4),
+    leechers: res.readUInt32BE(8),
+    seeders: res.readUInt32BE(12),
+    peers: group(res.slice(20), 6).map(address => {
+      return {
+        ip: address.slice(0, 4).join("."),
+        port: address.readUInt32BE(4)
+      };
+    })
+  };
+}
