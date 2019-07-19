78179389e3bc968a7c03b8e88e713a4b6911191f
Ben Lopata <benlopata@gmail.com>
2019-07-19 13:37:47 -0500

    Refactored to use srp modules and eliminate redundancy.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 "use strict";
-const fs = require("fs");
-const bencode = require("bencode");
-const tracker = require("./src/tracker");
-const torrentParser = require(process.argv[2]);
+const download = require("./src/download");
+const torrentParser = require("./src/torrent-parser");
 
-const torrent = torrentParser.open("./puppy.torrent");
+const torrent = torrentParser.open(process.argv[2]);
 
 try {
-  tracker.getPeers(torrent, peers => {
-    console.log(`list of peers: ${peers}`);
-  });
+  download(torrent, torrent.info.name);
 } catch (ex) {
   console.log(`error: ${ex.info}`);
 }
