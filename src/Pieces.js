eeaa1e171f3fe95d4662cea93bd6627d37f7ca4a
Ben Lopata <benlopata@gmail.com>
2019-07-19 11:38:57 -0500

    Added support blocks and queue integration.

--- src/Pieces.js
+++ src/Pieces.js
@@ -1,27 +1,39 @@
 "use strict";
 
+const tp = require("./torrent-parser");
+
 module.exports = class {
-  constructor(size) {
-    this.requested = new Array(size).fill(false);
-    this.received = new Array(size).fill(false);
-  }
-
-  addRequested(pieceIndex) {
-    this.requested[pieceIndex] = true;
-  }
-
-  addReceived(pieceIndex) {
-    this.received[pieceIndex] = true;
-  }
-
-  needed(pieceIndex) {
-    if (this.requested.every(i => i === true)) {
-      this.requested = this.received.slice();
+  constructor(torrent) {
+    function buildPiecesArray() {
+      const nPieces = torrent.info.pieces.length / 20;
+      const arr = new Array(nPieces).fill(null);
+      return arr.map(
+        (_, i) => new Array(tp.blocksPerPiece(torrent, i).fill(false))
+      );
     }
-    return !this.requested[pieceIndex];
+    this.requested = buildPiecesArray();
+    this.received = buildPiecesArray();
+  }
+
+  addRequested(pieceBlock) {
+    const blockIndex = pieceBlock.begin / tp.BLOCK_LEN;
+    this.requested[pieceBlock][blockIndex] = true;
+  }
+
+  addReceived(pieceBlock) {
+    const blockIndex = pieceBlock.begin / tp.BLOCK_LEN;
+    this.received[pieceBlock][blockIndex] = true;
+  }
+
+  needed(pieceBlock) {
+    if (this.requested.every(blocks => blocks.every(i => i))) {
+      this.requested = this._received.map(blocks => blocks.slice);
+    }
+    const blockIndex = pieceBlock.begin / tp.BLOCK_LEN;
+    return !this.requested[pieceBlock][blockIndex];
   }
 
   isDone() {
-    return this.received.every(i => i === true);
+    return this.received.every(i => i);
   }
 };
